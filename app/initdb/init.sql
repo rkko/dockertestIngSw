3e94a3c06d158b3add63dbb25f8300d62dacabb4
rkko <rkkouta@gmail.com>
2024-10-28 21:27:49 -0300

    FIX de contenedor no iniciandose Postgresql

--- app/initdb/init.sql
+++ app/initdb/init.sql
@@ -1,10 +1,25 @@
 -- init.sql
-CREATE DATABASE test;
 
+-- Crear la base de datos solo si no existe
+DO $$ 
+BEGIN 
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'test') THEN
+        EXECUTE 'CREATE DATABASE test';
+    END IF;
+END $$;
+
+-- Conectar a la base de datos 'test'
 \c test;
 
-CREATE TABLE test_table (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- Crear la tabla solo si no existe
+DO $$ 
+BEGIN 
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'test_table') THEN
+        EXECUTE 'CREATE TABLE test_table (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )';
+    END IF;
+END $$;
+
